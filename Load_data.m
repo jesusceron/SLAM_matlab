8e20a73545647c142929c98515989c0642306231
jesusceron <jdceronbravo@gmail.com>
2020-04-27 22:15:20 +0200

    ready to test

--- Load_data.m
+++ Load_data.m
@@ -1,96 +1,96 @@
 function [positions, Step_events, beac_rssi_fixed_filtered, beac_rssi_activity_filtered, beac_motion] = Load_data(participant)
 
 
-%% Load files
-Acc_Gyr_Beac_data= readtable(strcat('E:\Google Drive\IL_HAR_app_Matlab\dataset_synchronized\',int2str(participant),'.csv'));
+    %% Load files
+    Acc_Gyr_Beac_data= readtable(strcat('E:\Google Drive\IL_HAR_app_Matlab\dataset_synchronized\',int2str(participant),'.csv'));
 
-gyro_bias_i_c_s = [820 5500 4000 1 5000 3600 16120 1000 500 4000 1];
-gyro_bias_f_c_s = [1420 7500 8000 7500 6300 4300 17220 3000 2000 5000 3000];
-start_data_sample = [8080 8700 9500 8600 7000 5300 18800 9150 5600 5880 3900];
-final_data_sample = [63930 60200 88900 71200 72400 55450 114800 72600 70000 54630 62720];
+    gyro_bias_i_c_s = [820 5500 4000 1 5000 3600 16120 1000 500 4000 1];
+    gyro_bias_f_c_s = [1420 7500 8000 7500 6300 4300 17220 3000 2000 5000 3000];
+    start_data_sample = [8080 8700 9500 8600 7000 5300 18800 9150 5600 5880 3900];
+    final_data_sample = [63930 60200 88900 71200 72400 55450 114800 72600 70000 54630 62720];
 
-gyro_i_c_s = gyro_bias_i_c_s(participant); % initial calibration sample
-gyro_f_c_s = gyro_bias_f_c_s(participant); % initial calibration sample
+    gyro_i_c_s = gyro_bias_i_c_s(participant); % initial calibration sample
+    gyro_f_c_s = gyro_bias_f_c_s(participant); % initial calibration sample
 
-start_sample = start_data_sample(participant);
-final_sample = final_data_sample(participant);
-fs = 204.8; % IMU sample rate
+    start_sample = start_data_sample(participant);
+    final_sample = final_data_sample(participant);
+    fs = 204.8; % IMU sample rate
 
-%% Load data.
+    %% Load data.
 
-% Load IMU data
-acc_complete = table2array(Acc_Gyr_Beac_data(:,23:25));
-gyro_complete = table2array(Acc_Gyr_Beac_data(:,26:28));
+    % Load IMU data
+    acc_complete = table2array(Acc_Gyr_Beac_data(:,23:25));
+    gyro_complete = table2array(Acc_Gyr_Beac_data(:,26:28));
 
-% Load BEACONS data.
-beac_rssi_fixed = table2array(Acc_Gyr_Beac_data(:,...
-    {'RSSIs_beacon_1',...   % Coconut:  Room
-    'RSSIs_beacon_2',...    % Mint:     Kitchen
-    'RSSIs_beacon_3',...    % Ice:      Bathroom
-    'RSSIs_beacon_4',...    % Blueber:  Dining room
-    'RSSIs_beacon_5'}));    % P2:       Living room
+    % Load BEACONS data.
+    beac_rssi_fixed = table2array(Acc_Gyr_Beac_data(:,...
+        {'RSSIs_beacon_1',...   % Coconut:  Room
+        'RSSIs_beacon_2',...    % Mint:     Kitchen
+        'RSSIs_beacon_3',...    % Ice:      Bathroom
+        'RSSIs_beacon_4',...    % Blueber:  Dining room
+        'RSSIs_beacon_5'}));    % P2:       Living room
 
-beac_rssi_activity = table2array(Acc_Gyr_Beac_data(:,...
-    {'RSSIs_beacon_6',...   % P1:       Door
-    'RSSIs_beacon_7',...    % B2:       Toilet lid
-    'RSSIs_beacon_8',...    % B1:       Broom
-    'RSSIs_beacon_9',...    % G2:       Pitcher
-    'RSSIs_beacon_10'}));   % G1:       Hair brush
+    beac_rssi_activity = table2array(Acc_Gyr_Beac_data(:,...
+        {'RSSIs_beacon_6',...   % P1:       Door
+        'RSSIs_beacon_7',...    % B2:       Toilet lid
+        'RSSIs_beacon_8',...    % B1:       Broom
+        'RSSIs_beacon_9',...    % G2:       Pitcher
+        'RSSIs_beacon_10'}));   % G1:       Hair brush
 
-beac_motion = table2array(Acc_Gyr_Beac_data(:,{'motion_state_beacon_6',...
-    'motion_state_beacon_7',...
-    'motion_state_beacon_8',...
-    'motion_state_beacon_9',...
-    'motion_state_beacon_10'}));
+    beac_motion = table2array(Acc_Gyr_Beac_data(:,{'motion_state_beacon_6',...
+        'motion_state_beacon_7',...
+        'motion_state_beacon_8',...
+        'motion_state_beacon_9',...
+        'motion_state_beacon_10'}));
 
-%% Data pre-processing
-w = gyro_i_c_s : gyro_f_c_s;
+    %% Data pre-processing
+    w = gyro_i_c_s : gyro_f_c_s;
 
-acc = zeros(length(acc_complete),3);
-acc(:,1) = acc_complete(:,2); 
-acc(:,2) = acc_complete(:,3); % y = z
-acc(:,3) = acc_complete(:,1); 
+    acc = zeros(length(acc_complete),3);
+    acc(:,1) = acc_complete(:,2); 
+    acc(:,2) = acc_complete(:,3); % y = z
+    acc(:,3) = acc_complete(:,1); 
 
-gyr = zeros(length(gyro_complete),3);
-gyr(:,1) = gyro_complete(:,2); 
-gyr(:,2) = gyro_complete(:,3); 
-gyr(:,3) = gyro_complete(:,1); 
-gyr = deg2rad(gyr);
+    gyr = zeros(length(gyro_complete),3);
+    gyr(:,1) = gyro_complete(:,2); 
+    gyr(:,2) = gyro_complete(:,3); 
+    gyr(:,3) = gyro_complete(:,1); 
+    gyr = deg2rad(gyr);
 
-gravity=mean(sqrt(acc(w,1).^2 + acc(w,2).^2 + acc(w,3).^2));  % m/s^2
-acc_mean=[mean(acc(w,1)),mean(acc(w,2)),mean(acc(w,3))];
+    gravity=mean(sqrt(acc(w,1).^2 + acc(w,2).^2 + acc(w,3).^2));  % m/s^2
+    acc_mean=[mean(acc(w,1)),mean(acc(w,2)),mean(acc(w,3))];
 
-% Remove bias Gyro
-bias_gyr=[mean(gyr(w, 1)), mean(gyr(w, 2)), mean(gyr(w, 3))];
-gyr_unbiased=[gyr(:,1) - bias_gyr(1),...
-    gyr(:,2) - bias_gyr(2),...
-    gyr(:,3) - bias_gyr(3)];
+    % Remove bias Gyro
+    bias_gyr=[mean(gyr(w, 1)), mean(gyr(w, 2)), mean(gyr(w, 3))];
+    gyr_unbiased=[gyr(:,1) - bias_gyr(1),...
+        gyr(:,2) - bias_gyr(2),...
+        gyr(:,3) - bias_gyr(3)];
 
-acc = acc(start_sample:final_sample,:);
-gyr_unbiased = gyr_unbiased(start_sample:final_sample,:);
-beac_rssi_fixed = beac_rssi_fixed(start_sample:final_sample,:);
-beac_rssi_activity = beac_rssi_activity(start_sample:final_sample,:);
-beac_motion = beac_motion(start_sample:final_sample,:);
-%beac_rssi_fixed = [zeros(30,5); beac_rssi_fixed(start_sample:final_sample - 30,:)];
-%beac_rssi_activity = [zeros(30,5); beac_rssi_activity(start_sample:final_sample - 30,:)];
-%beac_motion = beac_motion(start_sample:final_sample - 30,:);
-
-
-
-% Filtering the beacons data in Python
-[beac_rssi_fixed_filtered, beac_rssi_activity_filtered] = rssiKF(beac_rssi_fixed,beac_rssi_activity);
+    acc = acc(start_sample:final_sample,:);
+    gyr_unbiased = gyr_unbiased(start_sample:final_sample,:);
+    beac_rssi_fixed = beac_rssi_fixed(start_sample:final_sample,:);
+    beac_rssi_activity = beac_rssi_activity(start_sample:final_sample,:);
+    beac_motion = beac_motion(start_sample:final_sample,:);
+    %beac_rssi_fixed = [zeros(30,5); beac_rssi_fixed(start_sample:final_sample - 30,:)];
+    %beac_rssi_activity = [zeros(30,5); beac_rssi_activity(start_sample:final_sample - 30,:)];
+    %beac_motion = beac_motion(start_sample:final_sample - 30,:);
 
 
-%% -----Step detection---------------------------
-idx_fig=20;
-[~,Step_events,~,~] = StepDetection_Acel(acc, 0, idx_fig);
 
-%stance_phase = load(strcat('E:\Google Drive\IL_HAR_app_Matlab\dataset_synchronized\stance_phase_p',int2str(participant),'.mat'));
-%Step_events = stance_phase.stance_phase(:,1)';
+    % Filtering the beacons data in Python
+    [beac_rssi_fixed_filtered, beac_rssi_activity_filtered] = rssiKF(beac_rssi_fixed,beac_rssi_activity);
 
 
-%% -------------------- Trajectory reconstruction ZUPT KF -----------------------%
-[positions]=ZUPT_KF(acc, gyr_unbiased, gravity, acc_mean, fs, Step_events, participant, idx_fig+4);
+    %% -----Step detection---------------------------
+    idx_fig=20;
+    [~,Step_events,~,~] = StepDetection_Acel(acc, 0, idx_fig);
+
+    %stance_phase = load(strcat('E:\Google Drive\IL_HAR_app_Matlab\dataset_synchronized\stance_phase_p',int2str(participant),'.mat'));
+    %Step_events = stance_phase.stance_phase(:,1)';
+
+
+    %% -------------------- Trajectory reconstruction ZUPT KF -----------------------%
+    [positions]=ZUPT_KF(acc, gyr_unbiased, gravity, acc_mean, fs, Step_events, participant, idx_fig+4);
 end
 
     
--- main.m
+++ main.m
@@ -8,7 +8,7 @@ N_PARTICLES = 500;
 N_LM = 10;
 LM_SIZE = 2;
 THRESHOLD_RESAMPLE = N_PARTICLES / 2;
-STD_PERSON_POSITION = 0.1;
+STD_PERSON_POSITION = 0.2;
 
 % [positions, step_events,beac_rssi_fixed_filtered, beac_rssi_activity_filtered, beac_motion] = Load_data(participant);
 % step_events = [1, step_events, length(beac_motion)]; % Add '1' and the lenght of the dataset as points of support
@@ -37,19 +37,26 @@ brush_moving = beac_motion(:,5);
 %% CREATION OF PARTICLES
 particles = [];
 trajectories = cell(1,N_PARTICLES);
+landmarks_figures = cell(1,N_LM);
 particles_weights = ones(1,N_PARTICLES) / N_PARTICLES;
 
 
 figure
 hold on
+grid on
 for i_particle=1:N_PARTICLES
     particles = [particles, Particle(N_PARTICLES,N_LM,LM_SIZE)];
-    trajectory = plot(nan,nan);
+    trajectory = plot(nan, nan, 'color', [.9 .9 .9]);
     trajectories{i_particle} = trajectory;
 end
+for i_landmark=1:N_LM
+    landmark_figure = plot(nan, nan,'d');
+    landmarks_figures{i_landmark} = landmark_figure;
+end
+
 particles = init_beacons_position(particles,N_PARTICLES);
-
-
+current_best_particle = N_PARTICLES;
+previous_best_particle = N_PARTICLES;
 
 %% MAIN LOOP
 x_prev = positions(1, 1);
@@ -63,8 +70,8 @@ for i_stride=2:length(positions)
     y = positions(i_stride, 2);
     for i_particle=1:N_PARTICLES
         
-        dist_x = (x - x_prev) + randn() * STD_PERSON_POSITION; % randn()=-1.2849
-        dist_y = (y - y_prev) + randn() * STD_PERSON_POSITION; % randn()=-0.1054
+        dist_x = (x - x_prev) + randn() * STD_PERSON_POSITION;
+        dist_y = (y - y_prev) + randn() * STD_PERSON_POSITION;
         particles(1,i_particle).X = particles(i_particle).X + dist_x;
         particles(1,i_particle).Y = particles(i_particle).Y + dist_y;
         
@@ -73,8 +80,19 @@ for i_stride=2:length(positions)
         
         trajectory_x_data = [particles(1,i_particle).T_x];
         trajectory_y_data = [particles(1,i_particle).T_y];
-        set(trajectories{i_particle},'XData',trajectory_x_data, 'YData',trajectory_y_data);
+
+        set(trajectories{i_particle},'XData',trajectory_x_data, 'YData',trajectory_y_data, 'color', [.9 .9 .9]);
+        
     end
+    
+    set(trajectories{current_best_particle},'color', 'b');
+    
+    % Plot the landmarks of the best particle.
+    for i_lm=1:N_LM
+        hold on
+        set(landmarks_figures{i_lm},'XData',particles(1,current_best_particle).Lm(i_lm,1), 'YData',particles(1,current_best_particle).Lm(i_lm,2));
+    end
+
     x_prev = x;
     y_prev = y;
     pause(0.05)
@@ -122,13 +140,13 @@ for i_stride=2:length(positions)
                             end
                             
                             % Beacon is still, update landmark
-                            [particles, particles_weights, trajectories] = ...
-                                update_resample(particles, rssi, j_rssi, particles_weights, N_PARTICLES, trajectories, THRESHOLD_RESAMPLE);
+                            [particles, particles_weights, trajectories, current_best_particle] = ...
+                                update_resample(particles, rssi, j_rssi, particles_weights, N_PARTICLES, trajectories, THRESHOLD_RESAMPLE, previous_best_particle);
                             
                         otherwise
                             
-                            [particles, particles_weights, trajectories] = ...
-                                update_resample(particles, rssi, j_rssi, particles_weights, N_PARTICLES, trajectories, THRESHOLD_RESAMPLE);
+                            [particles, particles_weights, trajectories, current_best_particle] = ...
+                                update_resample(particles, rssi, j_rssi, particles_weights, N_PARTICLES, trajectories, THRESHOLD_RESAMPLE, previous_best_particle);
                             
                     end
                 end
@@ -144,10 +162,11 @@ for i_lm=1:N_LM
     plot(particles(1,1).Lm(i_lm,1),particles(1,1).Lm(i_lm,2),'d');
 end
 
-function [particles, particles_weights, trajectories] = update_resample(particles, rssi, j_rssi, particles_weights, N_PARTICLES, trajectories, THRESHOLD_RESAMPLE)
+function [particles, particles_weights, trajectories, current_best_particle] = ...
+    update_resample(particles, rssi, j_rssi, particles_weights, N_PARTICLES, trajectories, THRESHOLD_RESAMPLE, previous_best_particle)
     
     % To update
-    [particles, particles_weights] = update(particles, rssi, j_rssi, particles_weights, N_PARTICLES);
+    [particles, particles_weights, current_best_particle] = update(particles, rssi, j_rssi, particles_weights, N_PARTICLES, previous_best_particle);
 
     % To resample
     neff = 1 / sum(sqrt(particles_weights));
@@ -160,7 +179,7 @@ function [particles, particles_weights, trajectories] = update_resample(particle
     end
 end
 
-function [particles, particles_weights] = update(particles, rssi, lm_id, particles_weights, N_PARTICLES)
+function [particles, particles_weights, current_best_particle] = update(particles, rssi, lm_id, particles_weights, N_PARTICLES, previous_best_particle)
     
     distance_rssi = 10 ^ ((rssi + 83) / -10 * 0.6);  % Calculate distance to beacon
     R = distance_rssi / 10;
@@ -210,9 +229,23 @@ function [particles, particles_weights] = update(particles, rssi, lm_id, particl
         %particles_weights(i_particle) = (particles_weights(i_particle) * ...
         %                                (1 / (covV * sqrt(2*pi))) * exp((-(distance_rssi - dist)^2) / (2 * covV^2))) + 1*exp(-300);
     end
+    
     particles_weights = particles_weights / sum(particles_weights);
+    best_particles = [];
+    biggest_weight = -1;
     for i_particle=1:N_PARTICLES
         particles(1,i_particle).W = particles_weights(i_particle);
+        
+        % Get the best particle (particle with the biggest weight)
+        if particles_weights(i_particle) >= biggest_weight
+            best_particles = [best_particles, i_particle];
+        end
+        if find(best_particles==previous_best_particle)
+            current_best_particle = previous_best_particle;
+        else
+            current_best_particle = best_particles(1); % for reproducibility the first item is always chosen.
+        end
+        
     end
 end
 
@@ -234,7 +267,7 @@ function [new_particles, new_particles_weights, trajectories] = resample_from_in
         trajectory_x_data = [new_particles(1,i_particle).T_x];
         trajectory_y_data = [new_particles(1,i_particle).T_y];
         set(trajectories{i_particle},'XData',trajectory_x_data, 'YData',trajectory_y_data);
-        plot(particles(1,1).Lm(i_lm,1),particles(1,1).Lm(i_lm,2),'d');
+
     end
     pause(0.05)
 end
